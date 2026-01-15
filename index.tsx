3b845a85ae85f554a2c89a13e5d58ddae67534c9
sertove-crypto <sertove@gmail.com>
2026-01-15 18:26:07 -0500

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -5,7 +5,7 @@ import App from './App';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error("No se pudo encontrar el elemento root para montar la aplicación.");
 }
 
 const root = ReactDOM.createRoot(rootElement);
